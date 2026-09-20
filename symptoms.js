484c957743578a8d40a1ae45ca3630d65caa0f62
soromolly <uliakor452@gmail.com>
2026-09-20 21:46:45 +0300

    Update symptoms.js

--- symptoms.js
+++ symptoms.js
@@ -440,7 +440,7 @@ export function getRandomEggEmbryoDiseaseId() {
 }
 
 export function rollEggEmbryoDisease() {
-    return Math.random() * 100 < 10 ? getRandomEggEmbryoDiseaseId() : null;
+    return Math.random() * 100 < 3 ? getRandomEggEmbryoDiseaseId() : null;
 }
 
 // ============================================================
